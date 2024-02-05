7e556ee4eb56094719aa600020733d7059db27f2
AlexD004 <dupre.alexandre04@gmail.com>
2024-02-05 16:32:13 +0100

    Header and Footer Structures

---
+++ src/components/Header.vue
@@ -0,0 +1,170 @@
+<script setup>
+
+  import ContactButton from '@/components/contactButton.vue';
+
+  /* --- FUNCITONS --- */
+
+    /* Toggle responsive menu on clik on open/close buttons */
+    function toggleResponsiveMenu() {
+        const navList = document.querySelector("nav");
+        navList.classList.toggle("open");
+        
+    }
+
+    /* Close responsive menu on click on navigation items to see the result and not have to close in a second time...*/
+    /* I don't use toggleResponsiveMenu because it open the responsive Nav on desktop...*/
+    function removeResponsiveMenu() {
+        const navList = document.querySelector("nav");
+        if(navList.classList.contains("open")){
+            navList.classList.remove("open");
+        }
+    }
+
+
+</script>
+
+<template>
+
+  <header>
+
+    <div class="wrapper">
+        <a href="/"><img id="logo" src="../assets/logo-alexEI.svg" alt="Logo d'Alexandre Dupré EI, un carré bleu dans lequel est écrit Alexandre en rose et jaune" /></a>
+      
+        <img id="menuBurger" src="../assets/menuBurger.svg" alt="Pictogramme d'ouverture du menu" v-on:click="toggleResponsiveMenu" />
+
+        <nav>
+            <img id="close" src="../assets/close.svg" alt="Pictogramme de fermeture du menu" v-on:click="toggleResponsiveMenu" />
+            <ul>
+                <li><a href="#services" v-on:click="removeResponsiveMenu">Services</a></li>
+                <li><a href="#portfolio" v-on:click="removeResponsiveMenu">Créations</a></li>
+                <li class="onlyResponsive" v-on:click="removeResponsiveMenu"><ContactButton /></li>
+            </ul>
+        </nav>
+        
+        <span class="onlyDesktop"><ContactButton /></span>
+    </div>
+
+  </header>
+
+</template>
+
+<style scoped>
+
+/* HEADER STYLES */
+header{
+  position: relative;
+  background: var(--c-beige);
+}
+header div.wrapper{
+  position: relative;
+  z-index: 20;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 20px;
+  height: 50px;
+
+  background: var(--c-beige);
+}
+header:before{
+  content:"";
+  position:absolute;
+  z-index: 1;
+  bottom: 0;
+  left:50%;
+  transform: translate(-50%,-40%);
+  opacity: .7;
+
+  width: 50%;
+  height: 10px;
+
+  background: #202020;
+  border-radius: 50%;
+  filter: blur(5px);
+}
+header #logo{
+  position: relative;
+  z-index:500;
+  transform: translate(0,40%);
+}
+
+/* -- STYLING LIST -- */
+ul{
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
+li{
+    margin: 0 25px;
+}
+li a{
+    font-weight: 500;
+    text-decoration: none;
+    color: var(--c-text-dark);
+    border-bottom:0 solid var(--c-pink);
+}
+li a:hover{
+    border-bottom: 2px solid var(--c-pink);
+}
+
+/* -- STYLING RESPONSIVE -- */
+
+/* Buttons Open/close */
+#menuBurger{
+    display: block;
+    cursor: pointer;
+}
+#close{
+    position: absolute;
+    right: 20px;
+    width: 25px;
+    height: auto;
+    cursor: pointer;
+}
+/* Styles and states of navigation list */
+nav{
+    display: none;
+}
+nav.open{
+    display: block;
+
+    position: fixed;
+    left:0;
+    top:0;
+    z-index: 50;
+
+    background: var(--c-beige);
+    width: 100%;
+    height: 100%;;
+    display: flex;
+    justify-content: center;
+    padding-top: 20px;
+}
+nav.open ul{
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+}
+nav.open li{
+    margin: 15px 0;
+}
+
+
+@media (min-width: 1024px) {
+
+#menuBurger,
+#close{
+    display: none;
+}
+nav{
+    display: block;
+}
+
+}
+
+</style>
---
+++ src/components/contactButton.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <a href="#contact" v-on:click="toggleResponsiveMenu">
+        <div class="contactButton">
+            Échangeons sur vos projets !
+        </div>
+    </a>
+
+</template>
+
+
+<style scoped>
+
+    .contactButton{
+        position: relative;
+        z-index:500;
+
+        background: var(--c-pink);
+        color: var(--c-text-light);
+        font-weight: 600;
+        border-radius: 10px;
+        padding: 10px;
+        max-height: 30px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        transition:.3s;
+    }
+
+    a{
+        text-decoration: none;
+    }
+
+    .contactButton:hover{
+        box-shadow: 3px 3px 0 var(--c-dark-blue);
+        padding: 15px;
+        transition:.3s;
+    }
+
+</style>
